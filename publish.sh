1fd4bc65df4d3924f54799a3206a18836d62ddc2
Taufiq <dhanani94@gmail.com>
2017-04-26 07:36:58 -0500

    updated publish script to work on multiple computers

--- publish.sh
+++ publish.sh
@@ -2,12 +2,24 @@
 
 if [[ $(git status -s) ]]
 then
-  echo "The working directory is dirty. Please commit any pending changes."
-  exit 1;
+    echo "The working directory is dirty. Please commit any pending changes."
+    exit 1;
 fi
 
-echo "Generating site"
+echo "Deleting old publication"
+rm -rf public
+mkdir public
+git worktree prune
+rm -rf .git/worktrees/public/
+
+echo "Checking out latest branch"
+git worktree add -B master public upstream/master
+
+echo "Removing existing files"
+rm -rf public/*
+
+echo "Generating site and content"
 hugo
 
-echo "Updating gh-pages branch"
-cd public && git add * && git commit -m "Publishing site changes to master (publish.sh)" && git push && cd ..
+echo "Publishing changes to master branch"
+cd public && git add --all && git commit -m "Publishing to master (publish.sh)"
