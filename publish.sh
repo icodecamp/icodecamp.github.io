8ce79bd92582b45cda9826e3e6ff9360fabb411d
Taufiq <dhanani94@gmail.com>
2017-04-28 09:53:10 -0500

    purple output yay

--- publish.sh
+++ publish.sh
@@ -1,27 +1,30 @@
 #!/bin/sh
 
+PURPLE='\033[0;35m'
+NORMAL='\033[0m'
+
 if [[ $(git status -s) ]]
 then
     echo "The working directory is dirty. Please commit any pending changes."
     exit 1;
 fi
 
-echo "Step 1: Deleting old publication"
+echo -e "${PURPLE}Step 1: Deleting old publication${NORMAL}"
 rm -rf public
 mkdir public
 git worktree prune
 rm -rf .git/worktrees/public/
 
-echo "Step 2: Setting up worktree to public content"
+echo -e "${PURPLE}Step 2: Setting up worktree to public content${NORMAL}"
 git worktree add -B master public master
 
-echo "Step 3: Removing existing files"
+echo -e "${PURPLE}Step 3: Removing existing files${NORMAL}"
 rm -rf public/*
 
-echo "Step 4: Generating site and content"
+echo -e "${PURPLE}Step 4: Generating site and content${NORMAL}"
 hugo
 
-echo "Step 5: Uploading changes to github"
+echo -e "${PURPLE}Step 5: Uploading changes to github${NORMAL}"
 cd public && git add --all && git commit -m "Publishing to master (publish.sh)" && git push origin master --force
 
-echo "Process complete"
+echo -e "${PURPLE}Process complete${NORMAL}"
